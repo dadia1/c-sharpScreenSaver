0f7baa7c930ef0addbba39ef84d5d708ff38a75e
dadia1 <47502747+dadia1@users.noreply.github.com>
2019-04-15 22:49:41 +0300

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScreenSaver
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Call the screen saver class to start program with "start()" function.
+            Screen_Saver.Start();
+        }
+    }
+}
